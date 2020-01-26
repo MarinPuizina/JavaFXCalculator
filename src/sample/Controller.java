1b78ab596f42caf899dfbcd979a38a4809c4e520
MarinPuizina <marinpuizina@hotmail.com>
2020-01-26 08:56:17 +0100

    Add JavaFXCalculator project

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void processNumbers(ActionEvent actionEvent) {
+
+
+
+    }
+
+    public void processOperators(ActionEvent actionEvent) {
+
+    }
+
+}
---
+++ src/sample/Model.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Model {
+
+    public float calculate(long numberOne, long numberTwo, String operator) {
+
+
+
+        return 0;
+    }
+
+}
